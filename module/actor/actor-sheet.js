f17b4f9a8a5395ce0774e7a56dc48c3b9779970a
Shaf074 <shafat.rashid@gmail.com>
2021-06-05 17:58:23 +1000

    fixed spellcasting

--- module/actor/actor-sheet.js
+++ module/actor/actor-sheet.js
@@ -336,7 +336,7 @@ export class ShazHackActorSheet extends ActorSheet {
     // console.log(this.actor.items.find(a => a.type == "sphere"));
     var cost = dataset.cost;
      //console.log(this.actor.items.find(a => a.type == "sphere" && a.data.name == dataset.sphere))
-     if(this.actor.items.find(a => a.type == "sphere" && a.data.name == dataset.sphere).data.data.reduction != null) {
+     if(this.actor.items.find(a => a.type == "sphere" && a.data.name == dataset.sphere) != null) {
       var adjustCost = parseInt(cost) + parseInt(this.actor.items.find(a => a.type == "sphere" && a.data.name == dataset.sphere).data.data.reduction);
      } else {
       var adjustCost = parseInt(cost);
